a2069e22221a0a546238c7e61fad71e31fe1ba36
Scott Richardson <srich1550@gmail.com>
2025-05-20 22:23:44 -0600

    added sequelize to connect to database

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const { sequelize } = require('sequelize');
+require('dotenv').config();
+
+//connect to database
+const sequelize = new sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
